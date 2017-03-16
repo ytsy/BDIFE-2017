fa1e62bfb9ac6f16a9b88d87327efa43de00143a
TANYOU <1024074941@qq.com>
2017-03-16 20:56:05 +0800

    零基础js-事件模型、事件绑定

--- binbin/index.js
+++ binbin/index.js
@@ -1,3 +1,9 @@
-/**
- * Created by tanyou on 2017/3/16.
- */
+(function() {
+    //三种监听事件的方式，1、HTML-onX属性。2、element.onclick()或者setAttribute("onclick","function(){}")。3、addeventListener()
+    var button = document.getElementById("button");
+    var weather = document.getElementById("aqi-input");
+    var output = document.getElementById("aqi-display");
+    button.addEventListener("click",function(){
+        output.innerHTML = weather.value;
+    },false);//只在冒泡阶段触发
+})();
